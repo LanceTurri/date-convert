61dba79ef219857960e546912be7cb54e3baddc8
Lance Turri <lance.turri@gmail.com>
2017-09-04 21:32:44 -0500

    Modularizing library code, cleaning up unit tests.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# date-translator
-Translates dates into sentence format.
+# Sentence Format Date Translator
+This library is meant for applications to take dates in a standard format (string or Date object) and convert them into human readable, sentence format. It's especially useful for ADA optimizations since screen readers do not read dates very well. 
+
+## Translate the Dates!
+There are two main functions in this library now, translating a date, and a translating a date with time. The method will take any string date or Date object and convert it to a sentence. This is an example of how to use it:
+
+```javascript
+convertDate("2015-03-25") // 'March twenty fifth two thousand fifteen'
+```
+
+Any format that can be read by the Date constructor will be able to be converted. Here are some common formats:
+
+```javascript
+convertDate('2010-03-25') // 'March twenty fifth two thousand ten'
+convertDate('07/04/1776') // 'July fourth seventeen seventy six'
+convertDate('August 29 2456') // 'August twenty ninth two thousand fifty six'
+convertDate('01/01/2000') // 'January first two thousand'
+convertDate('Tuesday March 15, 462') // 'March fifteenth four hundred sixty two'
+```
+
--- test/test.js
+++ test/test.js
@@ -1,92 +1,109 @@
-let assert = require('assert');
-let lib = require('../translate.js');
+const assert = require('assert');
+const translate = require('../core/translate');
+const dateStrings = require('../core/dateStrings');
+const parseDate = require('../lib/parseDate');
 
-describe('converts specific year digits', () => {
-    it('converts thousands digit properly', () => {
-        assert.equal(lib._interpretThousands('2000'), 'two thousand ');
-        assert.equal(lib._interpretThousands('1900'), '');
+const centuries = require('../lib/centuries');
+const decades = require('../lib/decades');
+const hundreds = require('../lib/hundreds');
+const thousands = require('../lib/thousands');
+
+describe('converts specific year digits', function() {
+    it('converts thousands digit properly', function() {
+        assert.equal(thousands('2000'), 'two thousand ');
+        assert.equal(thousands('1900'), '');
     });
 
-    it('converts hundreds digit properly', () => {
-        assert.equal(lib._interpretHundreds('1000'), '');
-        assert.equal(lib._interpretHundreds('1800'), 'eighteen hundred ');
-        assert.equal(lib._interpretHundreds('1900'), 'nineteen hundred ');
-        assert.equal(lib._interpretHundreds('2100'), 'one hundred ');
+    it('converts hundreds digit properly', function() {
+        assert.equal(hundreds('1000'), '');
+        assert.equal(hundreds('1800'), 'eighteen hundred ');
+        assert.equal(hundreds('1900'), 'nineteen hundred ');
+        assert.equal(hundreds('2100'), 'one hundred ');
     });
 
-    it('converts decade digits properly', () => {
-        assert.equal(lib._interpretDecade('2019'), 'nineteen');
-        assert.equal(lib._interpretDecade('2010'), 'ten');
-        assert.equal(lib._interpretDecade('2003'), 'three');
-        assert.equal(lib._interpretDecade('2000'), '');
+    it('converts decade digits properly', function() {
+        assert.equal(decades('2019'), 'nineteen');
+        assert.equal(decades('2010'), 'ten');
+        assert.equal(decades('2003'), 'three');
+        assert.equal(decades('2000'), '');
+    });
+
+    it('gracefully handles years greater than 9999', function() {
+        assert.throws(function() { parseDate('10000-01-01') }, Error);
     });
 });
 
+describe('converts standard date formats properly', function() {
+    const testValues = {
+        // TODO: Change these dates to major historical dates
+        isoDate: "2015-03-25",
+        shortDate: "03/25/2015",
+        longDate: "Mar 25 2015",
+        longDateReverse: "25 Mar 2015",
+        fullDate: "Wednesday March 25 2015",
+    }
 
-describe('converts full dates to sentences', () => {
-    describe('converts standard date formats properly', () => {
-        const testValues = {
-            // TODO: Change these dates to major historical dates
-            isoDate: "2015-03-25",
-            shortDate: "03/25/2015",
-            longDate: "Mar 25 2015",
-            longDateReverse: "25 Mar 2015",
-            fullDate: "Wednesday March 25 2015",
-        }
-
-        it('converts an ISO Date string (2015-03-25)', () => {
-            assert.equal(lib.convertDate(testValues.isoDate), 'March twenty fifth two thousand fifteen');
-        });
-
-        it('converts a short date string (03/25/2015)', () => {
-            assert.equal(lib.convertDate(testValues.shortDate), 'March twenty fifth two thousand fifteen');
-        });
-
-        it('converts a long date string (Mar 25 2015)', () => {
-            assert.equal(lib.convertDate(testValues.longDate), 'March twenty fifth two thousand fifteen');
-        });
-
-        it('converts a reversed long date string (25 Mar 2015)', () => {
-            assert.equal(lib.convertDate(testValues.longDateReverse), 'March twenty fifth two thousand fifteen');
-        });
-
-        it('converts a full date string (Wednesday March 25 2015)', () => {
-            assert.equal(lib.convertDate(testValues.fullDate), 'March twenty fifth two thousand fifteen');
-        });
+    it('converts an ISO Date string (2015-03-25)', function() {
+        assert.equal(translate(testValues.isoDate), 'March twenty fifth, two thousand fifteen');
     });
 
-    describe('it also converts special case years', () => {
-        const testValues = {
-            kubrick: "01/01/2001",
-            secondZero: "02/02/2010",
-            thirdZero: "03/03/2103",
-            century: "04/04/1900",
-            millenium: "05/05/2000",
-            random1: "06/06/2045"
-        }
+    it('converts a short date string (03/25/2015)', function() {
+        assert.equal(translate(testValues.shortDate), 'March twenty fifth, two thousand fifteen');
+    });
 
-        it('converts a year with two zeros (01/01/2001)', () => {
-            assert.equal(lib.convertDate(testValues.kubrick), 'January first two thousand one');
-        });
+    it('converts a long date string (Mar 25 2015)', function() {
+        assert.equal(translate(testValues.longDate), 'March twenty fifth, two thousand fifteen');
+    });
 
-        it('converts a year with a ten in the last pair (02/02/2010)', () => {
-            assert.equal(lib.convertDate(testValues.secondZero), 'February second two thousand ten');
-        });
+    it('converts a reversed long date string (25 Mar 2015)', function() {
+        assert.equal(translate(testValues.longDateReverse), 'March twenty fifth, two thousand fifteen');
+    });
 
-        it('converts a year with a zero in the third place (03/03/2103)', () => {
-            assert.equal(lib.convertDate(testValues.thirdZero), 'March third two thousand one hundred three');
-        });
+    it('converts a full date string (Wednesday March 25 2015)', function() {
+        assert.equal(translate(testValues.fullDate), 'March twenty fifth, two thousand fifteen');
+    });
 
-        it('converts a centennial year (04/04/1900)', () => {
-            assert.equal(lib.convertDate(testValues.century), 'April fourth nineteen hundred');
-        });
+    it('converts an ISO date object', function() {
+        assert.equal(translate(new Date('2015-03-25')), 'March twenty fifth, two thousand fifteen');
+    });
+});
 
-        it('converts a millenium year (05/05/2000)', () => {
-            assert.equal(lib.convertDate(testValues.millenium), 'May fifth two thousand');
-        });
+describe('it also converts special case years', function() {
+    const testValues = {
+        kubrick: "01/01/2001",
+        secondZero: "02/02/2010",
+        thirdZero: "03/03/2103",
+        century: "04/04/1900",
+        millenium: "05/05/2000",
+        random1: "06/06/2045",
+        subThousand: "06/06/465"
+    }
 
-        it('converts a short date string (06/06/2045)', () => {
-            assert.equal(lib.convertDate(testValues.random1), 'June sixth two thousand forty five');
-        });
+    it('converts a year with two zeros (01/01/2001)', function() {
+        assert.equal(translate(testValues.kubrick), 'January first, two thousand one');
+    });
+
+    it('converts a year with a ten in the last pair (02/02/2010)', function() {
+        assert.equal(translate(testValues.secondZero), 'February second, two thousand ten');
+    });
+
+    it('converts a year with a zero in the third place (03/03/2103)', function() {
+        assert.equal(translate(testValues.thirdZero), 'March third, two thousand one hundred three');
+    });
+    
+    it('converts a sub 1000 year', function() {
+        assert.equal(translate(testValues.subThousand), 'June sixth, four hundred sixty five');
+    });
+
+    it('converts a centennial year (04/04/1900)', function() {
+        assert.equal(translate(testValues.century), 'April fourth, nineteen hundred');
+    });
+
+    it('converts a millenium year (05/05/2000)', function() {
+        assert.equal(translate(testValues.millenium), 'May fifth, two thousand');
+    });
+
+    it('converts a short date string (06/06/2045)', function() {
+        assert.equal(translate(testValues.random1), 'June sixth, two thousand forty five');
     });
 });
